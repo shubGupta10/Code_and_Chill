abe5a07d9228d84c520dfe3ee744189ac50255e7
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-02 22:23:06 +0530

    2nd Commit

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -1,4 +1,4 @@
-// Create a function named generateMultiplier that takes a multiplier as an argument and returns a function. The returned function should take a number as an argument and multiply it by the multiplier. Use this function to create a doubler function and use it to double the number .
+// Q1:- Create a function named generateMultiplier that takes a multiplier as an argument and returns a function. The returned function should take a number as an argument and multiply it by the multiplier. Use this function to create a doubler function and use it to double the number .
 
 function multiplyGenerater(number){
     return function(x){
@@ -8,3 +8,18 @@ function multiplyGenerater(number){
 
 const result = multiplyGenerater(5);
 console.log("Here is your Result:- " + result(6));
+
+
+// Here is the code of :-
+
+// 1 : Formal and Normal Function
+
+function Table(number){
+    for(let i=1; i<=10; i++){
+        console.log(`${number} x ${i} = ${number * i}`);
+    }
+}
+
+Table(5);
+
+
