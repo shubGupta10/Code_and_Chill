184e3a8639bb08c6bcc2cac0659dcf6d8a2f42ba
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-15 10:36:04 +0530

    New Commit

---
+++ Java/100DaysofCode/Day_10/rotateArray.java
@@ -0,0 +1,36 @@
+// Q: Rotate a array clockwise 
+
+public class rotateArray{
+    public static void main(String[] args){
+
+        int [] arr = {10,20,30,40,50};
+        int n = arr.length;
+
+        System.out.println();
+        System.out.println("After before Rotation: ");
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+
+
+        Arrayrotation(arr, n); 
+        System.out.println();
+
+        System.out.println("Arrays after rotation: " );
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        
+    }
+
+    static void Arrayrotation(int [] arr, int n){
+        int temp = arr[n-1]; //storing the last element
+        for(int i=n-1; i>0; i--){ //runing our loop backward
+            arr[i] = arr[i-1];
+        }
+        arr[0] = temp; //placing the last element at beginning
+    }
+}
