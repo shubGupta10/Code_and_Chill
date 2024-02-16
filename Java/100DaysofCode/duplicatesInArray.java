ada16fb13e48fd2c91163f57092ba414634e06ce
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-16 10:55:47 +0530

    New Commit

--- Java/100DaysofCode/duplicatesInArray.java
+++ Java/100DaysofCode/duplicatesInArray.java
@@ -11,7 +11,7 @@ public class duplicatesInArray {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if(arr[i] == arr[j]){
-                    return arr[i];
+                    return i;
                 }
             }
         }
