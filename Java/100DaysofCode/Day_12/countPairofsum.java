1a64992f9a37ed6d3f7a2e3fc005e4a5dd3d554e
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-17 11:38:11 +0530

    New Commits

--- Java/100DaysofCode/Day_12/countPairofsum.java
+++ Java/100DaysofCode/Day_12/countPairofsum.java
@@ -15,7 +15,7 @@ public class countPairofsum {
         for(int i=0; i<arr.length; i++){
             for(int j=i+1; j<arr.length; j++){
                 if(arr[i] + arr[j] == k ){
-                    count = count + 1;
+                    count++;
                 }
             }
 
