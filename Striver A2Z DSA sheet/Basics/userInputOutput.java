a6eb055f26fc84180707a3b7021eafa4a04aac04
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-29 22:10:45 +0530

    new commit

---
+++ Striver A2Z DSA sheet/Basics/userInputOutput.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        char ch = in.next().charAt(0);
+
+        if(Character.isUpperCase(ch)){
+            System.out.println("1");
+        } else if(Character.isLowerCase(ch)){
+            System.out.println("0");
+        } else{
+            System.out.println("-1");
+        }
+
+    }
+}
