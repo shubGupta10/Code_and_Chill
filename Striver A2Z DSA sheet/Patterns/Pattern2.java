5392eebd9b1772a117d25661466f2d9c4208be7b
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-03-01 19:05:19 +0530

    New commit

--- Striver A2Z DSA sheet/Patterns/Pattern2.java
+++ Striver A2Z DSA sheet/Patterns/Pattern2.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class Pattern2 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        try (Scanner in = new Scanner(System.in)) {
+            int n = in.nextInt();
 
-        int n = in.nextInt();
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<=i; j++){
-                System.out.print("*");
+            for(int i=0; i<n; i++){
+                for(int j=0; j<=i; j++){
+                    System.out.print("*");
+                }
+                System.out.println(" ");
             }
-            System.out.println(" ");
         }
     }
 }
