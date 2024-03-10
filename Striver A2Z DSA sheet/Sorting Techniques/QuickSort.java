07c4689189c212b72b8345bcd0ee756e6888bd1d
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-03-11 00:22:21 +0530

    New Commit

--- Striver A2Z DSA sheet/Sorting Techniques/QuickSort.java
+++ Striver A2Z DSA sheet/Sorting Techniques/QuickSort.java
@@ -1,18 +1,39 @@
+import java.util.Arrays;
+
 public class QuickSort {
     public static void main(String[] args){
-
+        int [] arr = {74,3,38,12,24,56};
+        quickSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
 
     static void quickSort(int[] arr, int low, int high){
-        if(low>=high){
+        if(low >= high){
             return;
         }
 
         int s = low;
         int e = high;
-        int m = s + (e-s)/2;
+        int m = s + (e - s) / 2;
         int pivot = arr[m];
 
-        while()
+        while(s <= e){
+            while (arr[s] < pivot){
+                s++;
+            }
+            while (arr[e] > pivot){
+                e--;
+            }
+            if(s <= e){
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                s++;
+                e--;
+            }
+        }
+
+        quickSort(arr , low, e);
+        quickSort(arr, s, high);
     }
 }
