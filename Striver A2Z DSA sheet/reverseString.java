fd751ccde8591ad9e975d5f9287f0e28c1894450
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-03-21 11:18:51 +0530

    New Commit

---
+++ Striver A2Z DSA sheet/reverseString.java
@@ -0,0 +1,20 @@
+public class reverseString {
+    public static void main(String[] args) {
+        String input = "   Welcome   to    Coding   Ninjas   ";
+        String reversed = reverseTheString(input);
+        System.out.println(reversed);
+    }
+
+    public static String reverseTheString(String str) {
+        String[] words = str.trim().split("\\s+");
+
+        StringBuilder reversed = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+            if (i > 0) {
+                reversed.append(" "); // Append a space between words
+            }
+        }
+        return reversed.toString();
+    }
+}
