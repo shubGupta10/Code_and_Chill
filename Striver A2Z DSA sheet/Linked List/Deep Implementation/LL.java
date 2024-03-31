311a00bc54e47d456d2096e3eae4e62201699976
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-03-31 23:26:39 +0530

    New Commit

---
+++ Striver A2Z DSA sheet/Linked List/Deep Implementation/LL.java
@@ -0,0 +1,22 @@
+public class LL {
+
+    class Node{
+        String data;
+        Node next;
+
+
+        Node (String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+    //adding the element
+    
+
+
+    public static void main(String[] args) {
+        
+    }
+}
