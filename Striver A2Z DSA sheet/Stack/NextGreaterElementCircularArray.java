b434bca75bee1b936faa95722399f7743bad8687
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-13 10:48:08 +0530

    "Immediate Smaller"

---
+++ Striver A2Z DSA sheet/Stack/NextGreaterElementCircularArray.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+public class NextGreaterElementCircularArray {
+
+    public static void findNGE(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        int[] extendedArr = new int[2 * n];
+        for (int i = 0; i < 2 * n; i++) {
+            extendedArr[i] = arr[i % n];
+        }
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && extendedArr[i] > extendedArr[stack.peek()]) {
+                result[stack.pop()] = extendedArr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            result[stack.pop()] = -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(result[i]);
+            if (i < n - 1) {
+                System.out.print(" ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 5, 3, 4, 2};
+        findNGE(a);
+    }
+}
