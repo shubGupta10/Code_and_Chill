fa38b96b88e52b811602b3634a57b63d4b49ee99
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-29 23:57:11 +0530

    Closures

--- JavaScript/Closures.js
+++ JavaScript/Closures.js
@@ -1,3 +1,13 @@
 function outer(){
     const name = "Shubham"
+    function inner(){
+        console.log(name);
+    }
+    return inner; 
 }
+
+const newFn = outer();
+newFn();
+
+
+// ye jo return inner hai wo pura lexical scope ko return kr rha , means inner ko to kr rha saath me outer ko bhi.
