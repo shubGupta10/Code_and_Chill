166544066bf0b70358caeb1249e1f48a1337d599
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-07-11 19:23:05 +0530

    Solved array sorted

--- Striver A2Z DSA sheet/Arrays/Easy Questions/arraySorted.java
+++ Striver A2Z DSA sheet/Arrays/Easy Questions/arraySorted.java
@@ -1,16 +1,19 @@
 public class arraySorted {
-    public static void main(String[] args) {
-        int [] a = {23,67,12,45,34,87,10};
-        int n = a.length;
-        int result = isSorted(a, n);
-        System.out.println(result);
-    }   
-    
-    static int isSorted(int [] a, int n){
-        n = a.length;
-        for (int i = 0; i < n - 1; i++) {
-            if (a[i] > a[i + 1]) {
-                return 0; // Return 0 if any element is greater than the next element
+    public static void main(String[] args){
+        int [] arr  = {12,24,44,54,64};
+        int result = isSorted(arr);
+        if(result == 1){
+            System.out.println("Array is Sorted");
+        }else{
+            System.out.println("Array is not Sorted");
+        }
+    }
+
+
+    public static int isSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return 0;
             }
         }
         return 1;
