64634690ab2c249cb3c804a8a2ad3485fbf0953a
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-07-29 18:53:38 +0530

    kth Element into Array

--- Striver A2Z DSA sheet/Arrays/Medium Questions/Kth_Element_InArray.java
+++ Striver A2Z DSA sheet/Arrays/Medium Questions/Kth_Element_InArray.java
@@ -1,9 +1,21 @@
+import java.util.Arrays;
+
 public class Kth_Element_InArray {
     public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6,7,8,9};
+        int k = 2;
 
+        int result = findKthElement(arr, k);
+        System.out.println(result);
     }
 
     public static int findKthElement(int[] arr, int k) {
+        if(arr == null || arr.length == 0){
+            return 0;
+        }
+
+        Arrays.sort(arr);
+        return arr[arr.length - k];
 
     }
 }
