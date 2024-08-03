9ac26dd76c741fabc834e7149a876838f2261843
Shubham Kumar Gupta <132940559+shubGupta10@users.noreply.github.com>
2024-08-03 19:08:08 +0530

    Update stockSellandBuy.java

--- Striver A2Z DSA sheet/Arrays/Medium Questions/stockSellandBuy.java
+++ Striver A2Z DSA sheet/Arrays/Medium Questions/stockSellandBuy.java
@@ -6,6 +6,7 @@ public class stockSellandBuy {
         System.out.println(result);
     }
 
+    //custom function for besttimeandstocks
     static int bestTimeforStocks(int[] prices) {
         int n = prices.length;
         if (n < 2) {
