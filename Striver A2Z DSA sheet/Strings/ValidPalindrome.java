52d9f841af4ed28ff9e383efb636e956182f7781
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-04 14:44:06 +0530

    Valid Palindrome

---
+++ Striver A2Z DSA sheet/Strings/ValidPalindrome.java
@@ -0,0 +1,35 @@
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+
+        boolean result = isPalindrome(s);
+        System.out.println(result);
+    }
+
+    public static boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            char Chstart = Character.toLowerCase(s.charAt(start));
+            char chEnd = Character.toLowerCase(s.charAt(end));
+
+            if(!Character.isLetter(Chstart)){
+                start++;
+                continue;
+            }
+
+            if(!Character.isLetter(chEnd)){
+                end--;
+                continue;
+            }
+
+            if(chEnd != Chstart){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
