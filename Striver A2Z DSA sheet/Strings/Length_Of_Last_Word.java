bcb7e325e28975b53acd106e360be7434ff4efc0
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-10 16:48:45 +0530

    Fixes into Lenght of Last word

--- Striver A2Z DSA sheet/Strings/Length_Of_Last_Word.java
+++ Striver A2Z DSA sheet/Strings/Length_Of_Last_Word.java
@@ -4,6 +4,8 @@ public class Length_Of_Last_Word {
         int lastSpaceIndex = s.lastIndexOf(' ');
         return s.length() - lastSpaceIndex - 1;
     }
-
-
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(lengthOfLastWord(s));
+    }
 }
