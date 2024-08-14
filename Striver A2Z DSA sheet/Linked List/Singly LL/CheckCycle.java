9a8fadb33396c13cd30cf81184de59a55ccbc4c9
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-14 11:56:35 +0530

    Fixes in Check Cycle

--- Striver A2Z DSA sheet/Linked List/Singly LL/CheckCycle.java
+++ Striver A2Z DSA sheet/Linked List/Singly LL/CheckCycle.java
@@ -1,28 +1,47 @@
-public class CheckCycle{
+public class CheckCycle {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
 
-    public boolean HasCycle(ListNode head){
-        if(head == null && head.next == null){
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node2; 
+
+        CheckCycle solution = new CheckCycle();
+        boolean hasCycle = solution.hasCycle(node1);
+
+        System.out.println("The linked list has a cycle: " + hasCycle);
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
             return false;
         }
 
         ListNode slow = head;
         ListNode fast = head;
 
-        while (fast != null || fast.next != null) {
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
 
-            if(slow == fast){
+            if (slow == fast) {
                 return true;
             }
         }
-        return false;
+
+        return false; 
     }
 
-    public class ListNode{
+    public static class ListNode {
         int val;
         ListNode next;
-        ListNode (int x){
+        ListNode(int x) {
             val = x;
             next = null;
         }
