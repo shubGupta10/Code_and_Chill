56603a15146c82f89640c4e0742428a6da343a79
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-14 11:53:31 +0530

    checking Cycles on List

---
+++ Striver A2Z DSA sheet/Linked List/Singly LL/CheckCycle.java
@@ -0,0 +1,30 @@
+public class CheckCycle{
+
+    public boolean HasCycle(ListNode head){
+        if(head == null && head.next == null){
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null || fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode (int x){
+            val = x;
+            next = null;
+        }
+    }
+}
