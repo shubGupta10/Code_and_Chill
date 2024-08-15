3ea0b32fb63b688148b8281395ddad6f7d8da7b9
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-15 15:22:10 +0530

    Reverse A List

---
+++ DSA Practice/Linked List/Singly LL/ReverseAList.java
@@ -0,0 +1,48 @@
+public class ReverseAList {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        ListNode reversedHead = ReverseALinkedList(node1);
+        printList(reversedHead);
+    }
+
+    public static ListNode ReverseALinkedList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
