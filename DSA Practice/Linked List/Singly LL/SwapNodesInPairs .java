92cd0c93f50ae86565e8d358c7cdfbec668279c6
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-21 12:16:01 +0530

    Swap Nodes In Pairs

---
+++ DSA Practice/Linked List/Singly LL/SwapNodesInPairs .java
@@ -0,0 +1,41 @@
+public class SwapNodesInPairs {
+
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode current = dummy;
+
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+
+            first.next = second.next;
+            second.next = first;
+            current.next = second;
+
+            current = first;
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
