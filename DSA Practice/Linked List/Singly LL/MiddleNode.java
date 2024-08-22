1c39824a0d5d728e08e7af74a6feae201b9b9ad8
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-22 13:54:04 +0530

    Middle Node

--- DSA Practice/Linked List/Singly LL/MiddleNode.java
+++ DSA Practice/Linked List/Singly LL/MiddleNode.java
@@ -1,6 +1,14 @@
 public class MiddleNode{
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
 
+        MiddleNode solution = new MiddleNode();
+        head = solution.findMiddleNode(head);
+
+        printList(head);
     }
 
     public ListNode findMiddleNode(ListNode head){
