225a17e6217a37bf833063a7c307519936d484e4
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-23 12:56:13 +0530

    Remove Duplicates

--- DSA Practice/Linked List/Singly LL/RemoveDuplicates.java
+++ DSA Practice/Linked List/Singly LL/RemoveDuplicates.java
@@ -3,6 +3,36 @@ public class RemoveDuplicates {
 
     }
 
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            if(curr != null && curr.val == curr.next.val){
+                int val = curr.val;
+
+                while(curr != null && curr.val == val){
+                    curr = curr.next;
+                }
+
+                if(prev != null){
+                    prev.next = curr;
+                }else{
+                    head = curr;
+                }
+            }else{
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+
     public class ListNode{
         int val;
         ListNode next;
