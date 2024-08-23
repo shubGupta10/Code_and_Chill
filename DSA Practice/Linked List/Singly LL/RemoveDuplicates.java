a4c39848a427e42824e05915f6a9ebc03561d154
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-23 12:58:12 +0530

    Remove Duplicates (removing comments)

--- DSA Practice/Linked List/Singly LL/RemoveDuplicates.java
+++ DSA Practice/Linked List/Singly LL/RemoveDuplicates.java
@@ -1,6 +1,5 @@
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        // Create a linked list: 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
@@ -9,11 +8,9 @@ public class RemoveDuplicates {
         head.next.next.next.next.next = new ListNode(4);
         head.next.next.next.next.next.next = new ListNode(5);
 
-        // Create an instance of RemoveDuplicates and call deleteDuplicates
         RemoveDuplicates rd = new RemoveDuplicates();
         ListNode result = rd.deleteDuplicates(head);
 
-        // Print the result
         printList(result);
     }
 
