90688357c77661b0dd118735f556b92ab4a00b3f
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-24 13:56:16 +0530

    Linked List Cycle II

--- DSA Practice/Linked List/Singly LL/LinkedListCycleII.java
+++ DSA Practice/Linked List/Singly LL/LinkedListCycleII.java
@@ -3,7 +3,31 @@ public class LinkedListCycleII{
 
     }
 
+    public ListNode detectCycle(ListNode head){
+        if(head == null || head.next == null){
+            return null;
+        }
 
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                ListNode ptr1 = head;
+                ListNode ptr2 = slow;
+
+                while(ptr1 != ptr2){
+                    ptr1 = ptr1.next;
+                    ptr2 = ptr2.next;
+                }
+                return ptr1;
+            }
+        }
+        return null;
+    }
 
     public class ListNode {
         int val;
