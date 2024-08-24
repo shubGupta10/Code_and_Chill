1fb62ad87d2b7565bb451f91f9bfb43a6ab045f3
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-24 13:57:29 +0530

    Linked List Cycle II

--- DSA Practice/Linked List/Singly LL/LinkedListCycleII.java
+++ DSA Practice/Linked List/Singly LL/LinkedListCycleII.java
@@ -1,5 +1,19 @@
 public class LinkedListCycleII{
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        ListNode head = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(0);
+        ListNode node4 = new ListNode(-4);
+
+        head.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node2;
+
+        ListNode cycleStart = solution.detectCycle(head);
+
 
     }
 
