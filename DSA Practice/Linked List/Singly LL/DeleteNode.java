48a04785825f0e7927cf5f779d80f8f273ed0e88
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-26 14:32:10 +0530

    Delete Node

--- DSA Practice/Linked List/Singly LL/DeleteNode.java
+++ DSA Practice/Linked List/Singly LL/DeleteNode.java
@@ -1,10 +1,24 @@
 public class DeleteNode {
-    public static void main(String[] args) {
+    ListNode head = new ListNode(4);
+    head.next = new ListNode(5);
+    head.next.next = new ListNode(1);
+    head.next.next.next = new ListNode(9);
+        System.out.println("Original Linked List:");
 
-    }
+    deleteNode(head.next);
+
 
     public ListNode deleteTheNode(ListNode node){
         node.val = node.next.val;
         node.next = node.next.next;
+    }
+
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val = x;
+            next = null;
+        }
     }
 }
