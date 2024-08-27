00afc81e7ec4d9a1b38f7ada2da4bd5ec0b3a6fc
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-27 13:23:51 +0530

    Add two Numbers II: Second part - Add two Numbers fn

--- DSA Practice/Linked List/Singly LL/Add_two_Numbers_II.java
+++ DSA Practice/Linked List/Singly LL/Add_two_Numbers_II.java
@@ -17,6 +17,40 @@ public class Add_two_Numbers_II {
         return prev;
     }
 
+
+    public ListNode AddTwoNunber(ListNode l1, ListNode l2){
+        l1 = Reverse(l1);
+        l2 = Reverse(l2);
+
+        int sum = 0;
+        int carry = 0;
+
+        ListNode ans = new ListNode();
+
+        while(l1 != null || l2 != null){
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            ans.val = sum % 10;
+            carry = sum / 10;
+
+
+            ListNode newNode = new ListNode(carry);
+            newNode.next = ans;
+            ans = newNode;
+            sum = carry;
+        }
+
+        return carry == 0 ? ans.next : ans ;
+    }
+
     public class ListNode{
         int val;
         ListNode next;
