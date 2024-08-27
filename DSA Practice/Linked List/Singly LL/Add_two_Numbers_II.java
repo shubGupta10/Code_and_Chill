ab9a47c8a4dac619fa1eb1bf1f953eb8a052b5ff
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-27 13:18:10 +0530

    Add two Numbers II setup

---
+++ DSA Practice/Linked List/Singly LL/Add_two_Numbers_II.java
@@ -0,0 +1,14 @@
+public class Add_two_Numbers_II {
+    public static void main(String[] args) {
+
+    }
+
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val = x;
+            next = null;
+        }
+    }
+}
