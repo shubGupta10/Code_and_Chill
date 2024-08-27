69a6a88fbe6cec0de95246252b510c870cfd328f
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-27 13:20:08 +0530

    Add two Numbers II: first part - Reverse

--- DSA Practice/Linked List/Singly LL/Add_two_Numbers_II.java
+++ DSA Practice/Linked List/Singly LL/Add_two_Numbers_II.java
@@ -3,6 +3,20 @@ public class Add_two_Numbers_II {
 
     }
 
+    public ListNode Reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
     public class ListNode{
         int val;
         ListNode next;
