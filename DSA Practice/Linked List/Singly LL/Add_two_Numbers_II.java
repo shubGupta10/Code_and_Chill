81f2608fd84a5c2c257813d5fce1362d39360ac7
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-08-27 13:25:10 +0530

    Add two Numbers II: Main Fn and print List

--- DSA Practice/Linked List/Singly LL/Add_two_Numbers_II.java
+++ DSA Practice/Linked List/Singly LL/Add_two_Numbers_II.java
@@ -1,12 +1,28 @@
 public class Add_two_Numbers_II {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Add_two_Numbers_II solution = new Add_two_Numbers_II();
+
+        ListNode l1 = solution.new ListNode(7);
+        l1.next = solution.new ListNode(2);
+        l1.next.next = solution.new ListNode(4);
+        l1.next.next.next = solution.new ListNode(3);
+
+        ListNode l2 = solution.new ListNode(5);
+        l2.next = solution.new ListNode(6);
+        l2.next.next = solution.new ListNode(4);
+
+        ListNode result = solution.AddTwoNunber(l1, l2);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
     }
 
     public ListNode Reverse(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
-
 
         while(curr != null){
             ListNode temp = curr.next;
@@ -16,7 +32,6 @@ public class Add_two_Numbers_II {
         }
         return prev;
     }
-
 
     public ListNode AddTwoNunber(ListNode l1, ListNode l2){
         l1 = Reverse(l1);
@@ -41,20 +56,23 @@ public class Add_two_Numbers_II {
             ans.val = sum % 10;
             carry = sum / 10;
 
-
             ListNode newNode = new ListNode(carry);
             newNode.next = ans;
             ans = newNode;
             sum = carry;
         }
 
-        return carry == 0 ? ans.next : ans ;
+        return carry == 0 ? ans.next : ans;
     }
 
-    public class ListNode{
+    public class ListNode {
         int val;
         ListNode next;
-        ListNode(int x){
+        ListNode() {
+            val = 0;
+            next = null;
+        }
+        ListNode(int x) {
             val = x;
             next = null;
         }
