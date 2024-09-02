3118c928e923a6bb2e87d7fc13414f5274ea7ffe
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-09-02 17:49:54 +0530

    Reverse First K Elements

--- DSA Practice/Queue/ReverseFirstKElements.java
+++ DSA Practice/Queue/ReverseFirstKElements.java
@@ -26,5 +26,22 @@ public class ReverseFirstKElements {
         return q;
     }
 
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        queue.offer(4);
+        queue.offer(5);
+        queue.offer(6);
+        queue.offer(7);
 
+        int k = 3;
+
+        System.out.println("Original queue: " + queue);
+
+        Queue<Integer> resultQueue = reverseFirstK(queue, k);
+
+        System.out.println("Queue after reversing first " + k + " elements: " + resultQueue);
+    }
 }
