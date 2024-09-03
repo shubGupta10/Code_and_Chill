176c8430b0530160b72c4361bb2eb9783eeb682e
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-09-03 16:37:33 +0530

    Valid Parenthesis

---
+++ DSA Practice/Stack/ValidParenthesis.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class ValidParenthesis {
+        public boolean isValid(String s) {
+            Stack<Character> stack = new Stack<>();
+
+            for (char ch : s.toCharArray()) {
+                if (ch == '(' || ch == '{' || ch == '[') {
+                    stack.push(ch);
+                } else if (ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else if (ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                    stack.pop();
+                } else if (ch == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+            return stack.isEmpty();
+        }
+
+    public static void main(String[] args) {
+        ValidParenthesis validator = new ValidParenthesis();
+
+        System.out.println(validator.isValid("()"));
+        System.out.println(validator.isValid("()[]{}"));
+        System.out.println(validator.isValid("(]"));
+        System.out.println(validator.isValid("([)]"));
+        System.out.println(validator.isValid("{[]}"));
+        System.out.println(validator.isValid(""));
+    }
+    }
+
