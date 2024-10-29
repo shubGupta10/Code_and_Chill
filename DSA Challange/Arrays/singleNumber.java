2666244c85848c389aca9966b8a9352c631909cc
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-10-29 23:45:54 +0530

    added Comments

--- DSA Challange/Arrays/singleNumber.java
+++ DSA Challange/Arrays/singleNumber.java
@@ -3,6 +3,7 @@ public class singleNumber {
         int [] nums = {4,1,2,1,2,4,5};
         int ans = 0;
         for(int num : nums){
+            // we did here XOR operation, XOR which cancels out same type of elements
             ans = ans ^ num;
         }
         System.out.println(ans);
